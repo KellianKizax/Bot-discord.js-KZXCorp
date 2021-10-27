e47bd026a0a59acdc93732e35359361455c610a5
Kellian Goffic <57542020+KellianKizax@users.noreply.github.com>
2021-10-27 17:32:38 +0200

    slash commands dev guild deployement

---
+++ src/deploy-commands.js
@@ -0,0 +1,31 @@
+/*
+filename : deploy-commands.js
+description : Script used to register and update the slash commands for the bot.
+author : KellianKizax
+creation date : 27/10/2021
+*/
+
+// IMPORTS / REQUIRES
+require('dotenv').config();
+
+// GLOBAL CONSTANTS
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(process.env.CLIENT_TOKEN);
+
+// For guild only deployment
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
