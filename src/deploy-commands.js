65884ecf617c5d9d20ff87c66c636a866bf93bd9
Kellian Goffic <57542020+KellianKizax@users.noreply.github.com>
2021-10-27 18:13:09 +0200

    update guild deployment

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -9,19 +9,20 @@ creation date : 27/10/2021
 require('dotenv').config();
 
 // GLOBAL CONSTANTS
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-
 const clientId = process.env.CLIENT_ID;
 const guildId = process.env.GUILD_ID;
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(process.env.CLIENT_TOKEN);
 
