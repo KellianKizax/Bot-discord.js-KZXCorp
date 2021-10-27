c61276ef35bf50e24af46a3d1c52cc2e8629b1c6
Kellian Goffic <57542020+KellianKizax@users.noreply.github.com>
2021-10-27 18:52:12 +0200

    added command average

---
+++ src/commands/average.js
@@ -0,0 +1,33 @@
+/*
+filename : average.js
+description : Calculates the average of the given values.
+author : KellianKizax
+creation date : 27/10/2021
+*/
+
+// GLOBAL CONSTANT
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('average')
+		.setDescription('Calculates the average of the given values.')
+		.addStringOption(option =>
+			option.setName('values')
+				.setDescription('The list of values.')
+				.setRequired(true)),
+
+	async execute(interaction) {
+		let res = 0;
+		let values = interaction.options.getString('values');
+		values = values.split(' ');
+
+		values.forEach(element => {
+			res = res + parseFloat(element);
+		});
+
+		res = res / values.length;
+
+		await interaction.reply(`${res}`);
+	},
+};
